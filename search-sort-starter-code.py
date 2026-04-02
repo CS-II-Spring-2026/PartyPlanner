56c85b567c9c99b61d59d7769314eceb51cb19fb
Cory Houghton <cohawkeye@gmail.com>
2026-04-02 10:42:26 -0500

    Starter Code

---
+++ search-sort-starter-code.py
@@ -0,0 +1,7 @@
+names = [
+    "Olivia", "Liam", "Emma", "Noah", "Ava", "Sophia", "Mason", "Isabella", "Ethan", "Mia",
+    "Lucas", "Charlotte", "Amelia", "Harper", "Elijah", "Evelyn", "James", "Abigail", "Benjamin", "Emily",
+    "Alexander", "Ella", "Henry", "Scarlett", "Jackson", "Grace", "Sebastian", "Chloe", "Aiden", "Victoria",
+    "Matthew", "Riley", "Samuel", "Aria", "David", "Lily", "Joseph", "Aubrey", "Carter", "Zoey",
+    "Owen", "Penelope", "Wyatt", "Layla", "John", "Lillian", "Jack", "Nora", "Luke", "Hazel"
+]
